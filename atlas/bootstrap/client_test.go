edb8b93bc8d25e9c219bd229094cef3a2944c921
Robert Landers <landers.robert@gmail.com>
2024-12-28 19:34:42 +0100

    fix tests

--- atlas/bootstrap/client_test.go
+++ atlas/bootstrap/client_test.go
@@ -1,12 +1,19 @@
 package bootstrap_test
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"github.com/bottledcode/atlas-db/atlas"
 	"go.uber.org/zap"
 	"io"
-	"net"
+	"math/big"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/bottledcode/atlas-db/atlas/bootstrap"
 	"github.com/stretchr/testify/require"
@@ -15,6 +22,28 @@ import (
 
 type mockBootstrapServer struct {
 	bootstrap.UnimplementedBootstrapServer
+}
+
+func generateSelfSignedCert() tls.Certificate {
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Test Org"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	cert := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	key := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+
+	tlsCert, _ := tls.X509KeyPair(cert, key)
+	return tlsCert
 }
 
 func (m *mockBootstrapServer) GetBootstrapData(req *bootstrap.BootstrapRequest, stream bootstrap.Bootstrap_GetBootstrapDataServer) error {
@@ -48,7 +77,11 @@ func startMockServer(t *testing.T) (string, func()) {
 		atlas.Logger, _ = zap.NewDevelopment()
 	}
 
-	lis, err := net.Listen("tcp", "localhost:0")
+	lis, err := tls.Listen("tcp", "localhost:0", &tls.Config{
+		Certificates:       []tls.Certificate{generateSelfSignedCert()},
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"h2", "http/1.1"},
+	})
 	require.NoError(t, err)
 
 	s := grpc.NewServer()

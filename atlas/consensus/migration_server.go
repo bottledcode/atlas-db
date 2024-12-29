1fb5593c6dbbb819ed0022055f228bf2a3747615
Robert Landers <landers.robert@gmail.com>
2024-12-30 00:21:26 +0100

    rename files to better reflect what they do

---
+++ atlas/consensus/migration_server.go
@@ -0,0 +1,11 @@
+package consensus
+
+import "context"
+
+func (s *Server) StealTableOwnership(context.Context, *StealTableOwnershipRequest) (*StealTableOwnershipResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) WriteMigration(context.Context, *WriteMigrationRequest) (*WriteMigrationResponse, error) {
+	return nil, nil
+}
